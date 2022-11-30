4e97d19c13b8e065ee181c1475f7ed2bcac91691
oleg <oleg.ostrovski@gmail.com>
2022-11-30 17:57:51 +0300

    fixed lesson_04 SuperButton

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -12,10 +12,11 @@ import Stand from './Stand'
 const HW4 = () => {
     return (
         <div id={'hw4'}>
-            <div className={s2.hwTitle}>Homework #4</div>
-            {/*демонстрация возможностей компонент:*/}
+            <div className={s2.hwTitle}>
+                <h3 className={s2.container}>Homework #4</h3>
+            </div>
             <div className={s2.hw}>
-                <Stand />
+                <Stand/>
             </div>
         </div>
     )
