7b6f81bb223a48a6b3ca5e2e80772721f7da9529
oleg <oleg.ostrovski@gmail.com>
2023-01-03 15:21:17 +0300

    fix mistakes

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -8,22 +8,24 @@ import JuniorPlus from './pages/JuniorPlus'
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
+    JUNIOR_PLUS: '/plus-junior',
 }
 
 function Pages() {
     return (
         <div>
+            {/*Routes выбирает первый подходящий роут*/}
             <Routes>
-                <Route path={"/"} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
-                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-                <Route path={"/*"} element={<Error404/>}/>
+
+                <Route path={'/*'} element={<Error404/>}/>
             </Routes>
         </div>
     )
 }
-
 
 export default Pages
