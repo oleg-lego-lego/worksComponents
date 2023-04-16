f0e9d2e4a6a8e3c203d549fa014ee4979dae0411
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-04-16 20:12:08 +0300

    created logic for homeWorkReducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,12 +7,16 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let newState = [...state]
+            if (action.payload === 'up') {
+                newState.sort((a, b) => a.name > b.name ? 1 : -1);
+            } else if (action.payload === 'down') {
+                newState.sort((a, b) => a.name > b.name ? -1 : 1);
+            }
+            return newState
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
