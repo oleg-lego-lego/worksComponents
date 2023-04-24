9093a531f2c91da153bdc777a32fa62f3f117eda
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-04-24 16:40:52 +0300

    created type logic for themeReducer

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,23 @@
-const initState = {
+type StateType = {
+    themeId: number
+}
+
+const initState: StateType = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type ActionType = ChangeThemeIdType
+export const themeReducer = (state = initState, action: ActionType): StateType => { // fix any
     switch (action.type) {
-        // дописать
-
+        case "SET_THEME_ID": {
+            return {...state, themeId: action.id}
+        }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export type ChangeThemeIdType = ReturnType<typeof changeThemeId>
+export const changeThemeId = (id: number) => {
+    return {type: 'SET_THEME_ID', id} as const
+}
